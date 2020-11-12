a56a225c9a1472e1d3d79b797b3253a9e917a96a
dartungar <dartungar@gmail.com>
2020-11-12 21:22:21 +0300

    muted colors for dark theme

--- client/src/style/themes.js
+++ client/src/style/themes.js
@@ -19,13 +19,13 @@ const darkTheme = {
   BOX_SHADOW: "#29231d",
   TEXT: "#fffdfa",
   TEXT_LIGHT: "#7a7a7a",
-  PRIMARY: "#f7d063",
-  PRIMARY_LIGHT: "#ffecba",
-  INFO: "#fce5a2",
-  DANGER: "#ff8266",
-  DANGER_DARK: "#d64a2b",
-  SUCCESS: "#8ced9e",
-  SUCCESS_DARK: "#57b570",
+  PRIMARY: "rgba(247, 208, 99, 0.7)",
+  PRIMARY_LIGHT: "rgba(255, 236, 186, 0.7)",
+  INFO: "rgba(247, 208, 99, 0.7)",
+  DANGER: "rgba(255, 130, 102, 0.7)",
+  DANGER_DARK: "rgba(214, 74, 43, 0.7)",
+  SUCCESS: "rgba(140, 237, 158, 0.7)",
+  SUCCESS_DARK: "rgba(87, 181, 112, 0.7)",
 };
 
 export { lightTheme, darkTheme };
